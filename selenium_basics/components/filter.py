15b7dbfc5107d54032e2d1bb9aff94afe6c8a6db
chekirovstas <chekirovstas@gmail.com>
2023-11-07 02:32:44 +0100

    14 Python - Classes

--- selenium_basics/components/filter.py
+++ selenium_basics/components/filter.py
@@ -1,1 +1,19 @@
- 
+""" #3
+from components.base import Base
+
+class LeftFilter(Base):
+    def __init__(self, driver):
+        super().__init__(driver)"""
+
+#4
+from components.base import Base
+
+class LeftFilter(Base):
+    LOCATOR = "//*"
+
+    def __init__(self, driver):
+        super().__init__(driver)
+
+    def select_option(self, option, visible=False):
+        super().select_option(option, visible)
+        print(self.LOCATOR)
